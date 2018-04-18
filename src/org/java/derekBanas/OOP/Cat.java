3c40be026dca483f66676bfcb5bdffdbb1bb6435
alamnr <alamnr@gmail.com>
2018-04-18 12:59:24 +0600

    Commit for oop-01

---
+++ src/org/java/derekBanas/OOP/Cat.java
@@ -0,0 +1,14 @@
+package org.java.derekBanas.OOP;
+
+public class Cat extends Animal {
+	
+	public  Cat(){
+		super();
+		setSound("Meo");
+	}
+	public void move(){
+		System.out.println("Cat is running");
+	}
+	
+	
+}
---
+++ src/org/java/derekBanas/OOP/Dog.java
@@ -0,0 +1,20 @@
+package org.java.derekBanas.OOP;
+
+public class Dog extends Animal {
+	
+	public  Dog(){
+		super();
+		setSound("Bark");
+	}
+	public void move(){
+		System.out.println("Dog is running");
+	}
+	
+	public void digHole(){
+		System.out.println("Dog can dig hole.");
+	}
+	public void changeVar(int randNum){
+		randNum = 12;
+		System.out.println("Rand num in method -"+randNum);
+	}
+}
